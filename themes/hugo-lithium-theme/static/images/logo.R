5218e4d178287b8e55a2bcaecf083c99ab8da8ec
nanhung <nhsieh@cvm.tamu.edu>
2018-02-16 06:30:52 -0600

    update

--- themes/hugo-lithium-theme/static/images/logo.R
+++ themes/hugo-lithium-theme/static/images/logo.R
@@ -4,8 +4,7 @@ p <- ggplot(NULL, aes(x = 1, y = 1)) + ylim(0.8, 1.2) +
   annotate("text", 1, 1, label = temp, color="white",
            size = 90, parse = TRUE) +
   theme_bw() +
-  theme(panel.background = element_rect(fill = "red",
-                                        colour = "lightblue"),
+  theme(panel.background = element_rect(fill = "red"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.title = element_blank(), axis.ticks = element_blank(),
