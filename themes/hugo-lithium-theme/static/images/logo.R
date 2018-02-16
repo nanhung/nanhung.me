7bd525242e5e66d83937c2144be96b94d4c1aa22
nanhung <nhsieh@cvm.tamu.edu>
2018-02-15 23:51:12 -0600

    add logo.R

---
+++ themes/hugo-lithium-theme/static/images/logo.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+temp <- paste("hat(Pi)")
+p <- ggplot(NULL, aes(x = 1, y = 1)) + ylim(0.8, 1.2) +
+  annotate("text", 1, 1, label = temp, color="white",
+           fontface = "bold", size = 80, parse = TRUE) +
+  theme_bw() +
+  theme(panel.background = element_rect(fill = "red",
+                                        colour = "lightblue"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title = element_blank(), axis.ticks = element_blank(),
+        axis.text = element_blank())
+
+ggsave("logo.png", width = 5, height = 5, dpi = 96)
+
+
