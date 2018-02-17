d7f5bcc9f4dbb5496018f6f5da4d1ea617225562
nanhung <nhsieh@cvm.tamu.edu>
2018-02-16 18:03:15 -0600

    update logo

--- themes/hugo-lithium-theme/static/images/logo.R
+++ themes/hugo-lithium-theme/static/images/logo.R
@@ -3,10 +3,10 @@ temp <- paste("bold(sigma)")
 p <- ggplot(NULL, aes(x = 1, y = 1)) + ylim(0.8, 1.2) +
   annotate("text", 1, 1, label = temp, color="white",
            size = 99, parse = TRUE) +
-  theme_bw() +
+  #theme_bw() +
   theme(panel.background = element_rect(fill = "red"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
+        #panel.grid.major = element_blank(),
+        #panel.grid.minor = element_blank(),
         axis.title = element_blank(), axis.ticks = element_blank(),
         axis.text = element_blank())
 
